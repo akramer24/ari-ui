f1663ec6a6af7820a1eb523dcd6595845df13e94
Ari Kramer <arikramer24@gmail.com>
2019-09-10 19:33:20 -0400

    random

--- src/lib/Checkbox/Checkbox.js
+++ src/lib/Checkbox/Checkbox.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import classNames from 'classnames';
 import { omitBy, isUndefined } from 'lodash';
+import PropTypes from 'prop-types';
 import withForm from '../../hocs/withForm';
 
 const Checkbox = ({
   autoFocus,
   checked,
   className,
-  defaultChecked,
+  disabled,
   id,
   name,
   onChange,
   required,
 }) => {
-  const passthroughToCheckbox = omitBy({ checked, id, name, onChange }, isUndefined);
+  const passthroughToCheckbox = omitBy({
+    autoFocus,
+    checked,
+    disabled,
+    id,
+    name,
+    onChange,
+    required
+  }, isUndefined);
 
   return (
     <input
-      autoFocus={autoFocus}
       className={classNames('ari-ui-checkbox', { [className]: className })}
-      defaultChecked={defaultChecked}
-      required={required}
       type="checkbox"
       {...passthroughToCheckbox}
     />
@@ -28,7 +34,38 @@ const Checkbox = ({
 }
 
 Checkbox.defaultProps = {
-  defaultChecked: false
+  /**
+   * Determines whether or not Checkbox has focus on mount.
+   */
+  autoFocus: PropTypes.bool,
+  /**
+   * Determines whether or not Checkbox is checked.
+   */
+  checked: PropTypes.bool,
+  /**
+   * CSS class(es) applied to Checkbox.
+   */
+  className: PropTypes.string,
+  /**
+   * Determines whether or not Checkbox is disabled
+   */
+  disabled: PropTypes.bool,
+  /**
+   * CSS id applied to Checkbox.
+   */
+  id: PropTypes.string,
+  /**
+   * Name of Checkbox.
+   */
+  name: PropTypes.string,
+  /**
+   * Callback griggered on check/uncheck.
+   */
+  onChange: PropTypes.func,
+  /**
+   * Determines whether or not Checkbox is required.
+   */
+  required: PropTypes.bool
 }
 
 export default withForm(Checkbox);
